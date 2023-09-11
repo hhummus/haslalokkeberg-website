faa686a2cd684f7c673657276d67359724179c7c
Helena <Helena_L93@hotmail.com>
2023-09-11 19:13:08 +0200

    updated navbar to burger menu. repsonsive small-large

--- src/components/global/Navbar.js
+++ src/components/global/Navbar.js
@@ -1,23 +1,49 @@
-import { Link } from "react-router-dom";
 import "../../css/navbar.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
-function Navbar() {
+function Navbarr() {
+  const [navbar, setNavbar] = useState(false);
+
+  const navbarOnScroll = () => {
+    if (window.scrollY >= 65) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", navbarOnScroll);
+
   return (
-    <div className="navbar">
-      <div className="container">
-        <div className="navbar-logo row">
-          <Link to="/">Hasla-Løkkeberg</Link>
-        </div>
-        <div className="row">
-          <ul className="navbar-menu">
-            <li>
-              <Link to="/om-oss">Om oss</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className={navbar ? "navbar active" : "navbar"}
+      >
+        <Link to="/" className="navbar-logo">
+          Hasla-Løkkeberg Agentur
+        </Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <div className="nav-dropdown-divider"></div>
+          <Nav className="mr-auto">
+            <Link to="/om-oss" className="nav-dropdown">
+              Bli kjent med oss
+            </Link>
+            <Link to="/kontakt-oss" className="nav-dropdown">
+              Kontakt oss
+            </Link>
+            <Link to="/prislister" className="nav-dropdown">
+              Prislister
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 }
 
-export default Navbar;
+export default Navbarr;
