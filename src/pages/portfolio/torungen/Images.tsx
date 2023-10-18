2ca94e046017449a6ee23a6460fa79ecde05bfee
Helena <Helena_L93@hotmail.com>
2023-10-18 13:44:18 +0200

    added 2 more photos for torungen

--- src/pages/portfolio/torungen/Images.tsx
+++ src/pages/portfolio/torungen/Images.tsx
@@ -3,16 +3,22 @@ import torungenImgTwo from "../../../img/portfolio/torungen/torungen-2.JPG";
 import torungenImgThree from "../../../img/portfolio/torungen/torungen-3.JPG";
 import torungenImgFour from "../../../img/portfolio/torungen/torungen-4.JPG";
 import torungenImgFive from "../../../img/portfolio/torungen/torungen-5.JPG";
+import torungenImgSix from "../../../img/portfolio/torungen/torungen-6.JPG";
+import torungenImgSeven from "../../../img/portfolio/torungen/torungen-7.JPG";
 
 
 
 
 const torungenImagesArray = [
+   torungenImgSeven,
+   torungenImgSix,
    torungenImgOne,
    torungenImgTwo,
    torungenImgThree,
    torungenImgFour,
-   torungenImgFive
+   torungenImgFive,
+
+ 
   
 ]
 
