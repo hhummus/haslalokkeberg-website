d2423676b6dc6452aa68aa0fcd6a0d85e13fd034
Helena <Helena_L93@hotmail.com>
2023-11-16 12:23:21 +0100

    width of landingPageContainer changes on bigger screen

--- src/pages/portfolio/wilder-kitchen/Images.tsx
+++ src/pages/portfolio/wilder-kitchen/Images.tsx
@@ -1,10 +1,10 @@
-import wkImgOne from "../../../img/portfolio/wilder-kitchen/wk-1.JPG";
-import wkImgTwo from "../../../img/portfolio/wilder-kitchen/wk-2.JPG";
-import wkImgThree from "../../../img/portfolio/wilder-kitchen/wk-3.JPG";
-import wkImgFour from "../../../img/portfolio/wilder-kitchen/wk-4.JPG";
-import wkImgFive from "../../../img/portfolio/wilder-kitchen/wk-5.JPG";
-import wkImgSix from "../../../img/portfolio/wilder-kitchen/wk-6.JPG";
-import wkImgSeven from "../../../img/portfolio/wilder-kitchen/wk-7.JPG"
+import wkImgOne from "../../../img/portfolio/wilder-kitchen/wk-1-optimized.jpg";
+import wkImgTwo from "../../../img/portfolio/wilder-kitchen/wk-2-optimized.jpg";
+import wkImgThree from "../../../img/portfolio/wilder-kitchen/wk-3-optimized.jpg";
+import wkImgFour from "../../../img/portfolio/wilder-kitchen/wk-4-optimized.jpg";
+import wkImgFive from "../../../img/portfolio/wilder-kitchen/wk-5-optimized.jpg";
+import wkImgSix from "../../../img/portfolio/wilder-kitchen/wk-6-optimized.jpg";
+import wkImgSeven from "../../../img/portfolio/wilder-kitchen/wk-7-optimized.jpg"
 
 const wilderImagesArray = [
   wkImgOne,
