6a3b936a7b91711c542dcdec4f20890aafdbb26d
Helena <Helena_L93@hotmail.com>
2024-04-07 13:41:52 +0200

    updated go back arrow

--- src/components/global/GoBack.tsx
+++ src/components/global/GoBack.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 import "../../css/backOne.css";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesLeft} from '@fortawesome/free-solid-svg-icons';
 
 function GoBack () {
   let navigate = useNavigate();
 
   return (
     <button className= "backOne" onClick={() => navigate(-1)}>
-    &#8592; Tilbake
+     <FontAwesomeIcon icon={faAnglesLeft} size="sm" /> Tilbake
     </button>
   );
 }
