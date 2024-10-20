338c41c6aa82c618da7406cd2e2f9d0dc7ab6847
Helena <Helena_L93@hotmail.com>
2024-10-20 12:31:01 +0200

    updated footer

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,34 +1,28 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import "../../css/footer.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-
-
 
 function Footer() {
   return (
     <div className="globalFooter">
-      
-
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12 col-md-6 globalFooterCopyright">
-              <p>© 2024 haslalokkeberg</p>
-            </div>
-            <div className=" col-sm-12 col-md-6 soMe">
-            <Link to="https://www.instagram.com/haslalokkeberg/" target="_blank"><FontAwesomeIcon icon={faInstagram} /></Link>
-            <Link to="/kontakt-oss"><FontAwesomeIcon icon={faEnvelope} /></Link>
-          </div>        
+      <div className="divider"></div>
+      <div className="container">
+        <div className="row soMe">
+          <div className="col-12 instagram">
+            <Link to="https://www.instagram.com/haslalokkeberg/" target="_blank">Følg oss på Instagram</Link>
+          </div>
+          <div className="col-12 contact">
+            <Link to="/kontakt-oss">Send oss e-post</Link>
           </div>
         </div>
-        <div className="container">
         <div className="row">
-          
+          <div className="col-12 containerAllRights">
+          <p className="allRights">© 2024 Hasla-Løkkeberg Agentur</p>
+          </div>
+       
         </div>
       </div>
-      </div>
+    </div>
   );
 }
 
